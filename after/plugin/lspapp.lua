89dbaa46911d8f7d5116b24b30d69dd03571c8d5
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-08-23 00:58:40 +0100

    some lspconfig

---
+++ after/plugin/lspapp.lua
@@ -0,0 +1,8 @@
+-- if true then return end
+vim.fn.sign_define('LightBulbSign', { text = ' ', texthl = 'WarningMsg', linehl = '', numhl = '' })
+require('nvim-lightbulb').update_lightbulb()
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua require('nvim-lightbulb').update_lightbulb()]]
+
+require('lsp_lines').setup()
+vim.keymap.set('', ',ll', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
+
