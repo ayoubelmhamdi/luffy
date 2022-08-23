470e1571e0b5c68fe5705be7401edb5a17a17f6a
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-08-23 02:42:01 +0100

    up

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,1 +1,7 @@
-require("luasnip.loaders.from_lua").lazy_load()
+--
+--{{{ # Load the lua snippets
+require('luasnip.loaders.from_lua').lazy_load()
+--}}}
+--{{{ # Create Command!: LuaSnipEdit
+vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]
+--}}}
