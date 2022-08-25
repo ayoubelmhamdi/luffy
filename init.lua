598542e99e9e25f94e231f3125e533aea2e43451
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-08-26 00:14:55 +0100

    rescheme

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ require('impatient')
 
 vim.g.mapleader = ' '
 vim.opt.termguicolors = true
-vim.o.background = 'dark'
-vim.cmd 'colorscheme gruvbox'
+-- vim.o.background = 'dark'
+-- vim.cmd 'colorscheme gruvbox'
 
 vim.cmd 'set cole=1'
 vim.cmd 'set switchbuf=usetab'
