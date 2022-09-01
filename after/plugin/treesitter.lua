10512eaaaa5e427cc27a59cc7367082c30c8bc4e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-09-01 23:25:30 +0100

    up

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,6 +6,7 @@ i.update { with_sync = true }
 treesitter.setup {
   ensure_installed = {
     'c',
+    'cpp',
     'comment',
     'dart',
     'html',
@@ -88,6 +89,24 @@ treesitter.setup {
       keymaps = {
         smart_rename = 'grr',
       },
+    },
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
     },
   },
   --
