46328668559270b174bce7d37e010e88c58248fe
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-09-14 11:04:04 +0100

    use lsp_progress and filename by root

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,37 +1,24 @@
 -- if true then
 --   return
 -- end
-
-local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-  return function(str)
-    local win_width = vim.fn.winwidth(0)
-    if hide_width and win_width < hide_width then
-      return ''
-    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-      return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
-    end
-    return str
-  end
+local myfilename = function()
+    -- print("aa" .. vim.fn.expand('@%'))
+    -- print("aa" .. expand('@%'))
+    local path= vim.fn.expand('%p')
+    return  path
 end
 
-local lsp_status = function()
-  local msg = ''
-  if #vim.lsp.get_active_clients() > 0 then
-    msg = '[LSP]'
+local lsp_server = function()
+  local msg = 'No Active Lsp'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
   end
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
-    msg = msg .. '‹' .. client.name .. '›'
-    for _, progress in pairs(client.messages.progress) do
-      if not progress.done then
-        msg = progress.title
-        if progress.message then
-          msg = msg .. ' ' .. progress.message
-        end
-        if progress.percentage then
-          msg = string.format('%s%2d%s', '⸢', progress.percentage, '%%⸥')
-        end
-        return msg
-      end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
     end
   end
   return msg
@@ -46,29 +33,32 @@ require('lualine').setup {
   sections = {
     lualine_a = { '' },
     lualine_b = { 'branch' },
-    lualine_c = {
+    lualine_c = { myfilename },
+    -- lualine_c = {
+    --   {
+    --     'filename',
+    --     file_status = true, -- Displays file status (readonly status, modified status)
+    --     path = 2, -- 0: Just the filename
+    --     shorting_target = 80, -- Shortens path to leave 40 spaces in the window
+    --     symbols = {
+    --       modified = '[+]', -- Text to show when the file is modified.
+    --       readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+    --       unnamed = '[No Name]', -- Text to show for unnamed buffers.
+    --     },
+    --   },
+    -- },
+    lualine_x = { '' },
+    lualine_y = {
       {
-        'filename',
-        file_status = true, -- Displays file status (readonly status, modified status)
-        path = 2, -- 0: Just the filename
-        shorting_target = 80, -- Shortens path to leave 40 spaces in the window
-        symbols = {
-          modified = '[+]', -- Text to show when the file is modified.
-          readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
-          unnamed = '[No Name]', -- Text to show for unnamed buffers.
-        },
+        lsp_server,
+        icon = '',
+        -- If you want to give the LSP name color then use the line below and
+        -- change the hex code to the color you want:
+        -- color = {fg = '#ffffff', gui = 'bold'}
       },
+    'diagnostics',
+    'lsp_progress'
     },
-    lualine_x = {
-      {
-        -- function()
-        --   return require('lsp-status').status()
-        -- end,
-        lsp_status,
-        fmt = trunc(120, 20, 60),
-      },
-    },
-    lualine_y = { 'progress' },
-    lualine_z = { 'diagnostics' },
+    lualine_z = {  'progress' },
   },
 }
