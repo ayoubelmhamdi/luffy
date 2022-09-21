82d00b61fd7ef42049b205ad02afba01a44c71bd
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-09-21 20:13:46 +0100

    fix lamp code-action

--- after/plugin/lspapp.lua
+++ after/plugin/lspapp.lua
@@ -1,8 +1,17 @@
 -- if true then return end
-vim.fn.sign_define('LightBulbSign', { text = ' ', texthl = 'WarningMsg', linehl = '', numhl = '' })
-require('nvim-lightbulb').update_lightbulb()
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua require('nvim-lightbulb').update_lightbulb()]]
-
--- require('lsp_lines').setup()
-vim.keymap.set('', ',ll', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
-
+require('nvim-lightbulb').setup {
+  virtual_text = {
+    enabled = false,
+    text = '💡',
+  },
+  status_text = {
+    enabled = false,
+    text = '💡',
+    text_unavailable = '',
+  },
+  autocmd = {
+    enabled = true,
+    pattern = { '*' },
+    events = { 'CursorHold', 'CursorHoldI' },
+  },
+}
