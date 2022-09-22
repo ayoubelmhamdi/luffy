d2b0144d68ff76b190630749207fe510c3be6116
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-09-22 15:10:44 +0100

    improve capabilites

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,6 +1,7 @@
 -- if true then return end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
 
