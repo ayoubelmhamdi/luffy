0e112b60c2fc188f64b17ba620c91034fa5fed94
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-01 19:21:50 +0100

    up

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -1,26 +1,39 @@
-vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
-  pattern = {"*.html", "*.md", "*.js", "*.tex", "*.c", "*.lua", "*.h", "*.dart", "*.rs" ,"*.sh", "*.tex", "*.toml", "*.yaml"},
-  callback = function ()
-      vim.b.auto_save = 1
-  end  ,
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+  pattern = {
+    '*.css',
+    '*.html',
+    '*.js',
+    '*.md',
+    '*.tex',
+    '*.c',
+    '*.h',
+    '*.lua',
+    '*.dart',
+    '*.rs',
+    '*.sh',
+    '*.toml',
+    '*.yaml',
+  },
+  callback = function()
+    vim.b.auto_save = 1
+  end,
 })
 
-vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
-  pattern = {"*.dart"},
-  callback = function ()
-    os.execute('killflutter')
-  end  ,
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+  pattern = { '*.dart' },
+  callback = function()
+    os.execute 'killflutter'
+  end,
 })
 
 -- highlight text on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank {
-            higroup = "IncSearch",
-            timeout = 300,
-            on_visual = true,
-        }
-    end,
+vim.api.nvim_create_autocmd('TextYankPost', {
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = 'IncSearch',
+      timeout = 300,
+      on_visual = true,
+    }
+  end,
 })
-
