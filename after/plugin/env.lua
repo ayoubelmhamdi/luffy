b21381dfbbd35dce9776a12139455d961fe3a9fe
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-02 18:26:22 +0100

    fix permision when using finfile, so i use io.open

--- after/plugin/env.lua
+++ after/plugin/env.lua
@@ -1,7 +1,20 @@
-local vimrc = vim.fn.findfile('run_buf.lua', ',;')
-if string.len(vimrc) >= 1 then
-    require(string.sub(vimrc,1,-5))
-end
+-- local vimrc = vim.fn.findfile('run_buf.lua', ',;')
+-- if string.len(vimrc) >= 1 then
+--   print('vimrc: ' .. vimrc)
+--   require(string.sub(vimrc, 1, -5))
+-- end
 -- print('run_buf.lua: ' .. vimrc)
 -- else
---     print("else1")
+
+local vimrc = 'run_buf.lua'
+local root = vim.fn.expand '%:p:h'
+local file = io.open(vimrc, 'r')
+if file ~= nil then
+  io.close(file)
+  print('found: ' .. root .. '/' .. vimrc)
+  vim.cmd('source' .. root .. '/' .. vimrc)
+  -- require(string.sub(file, 1, -5))
+  return true
+else
+  return false
+end
