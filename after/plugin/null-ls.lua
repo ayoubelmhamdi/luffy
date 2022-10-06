973b95ada3ea18ea5a49ee1ea838d404181b4944
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-06 07:07:11 +0100

    up

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,4 +1,4 @@
--- if true then return end
+if true then return end
 local null_ls_status_ok, null_ls = pcall(require, 'null-ls')
 if not null_ls_status_ok then
   print('null-ls error in: ' .. vim.fn.expand '%@')
