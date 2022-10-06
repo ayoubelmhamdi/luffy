cc9efc4c435f9affdf3daffdd70c71130685631b
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-06 09:42:25 +0100

    (add): overSeer: task runner

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,61 +4,57 @@
 local myfilename = function()
     -- print("aa" .. vim.fn.expand('@%'))
     -- print("aa" .. expand('@%'))
-    local path= vim.fn.expand('%p')
-    return  path
+    local path = vim.fn.expand('%p')
+    return path
 end
 
 local lsp_server = function()
-  local msg = 'No Active Lsp'
-  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-  local clients = vim.lsp.get_active_clients()
-  if next(clients) == nil then
-    return msg
-  end
-  for _, client in ipairs(clients) do
-    local filetypes = client.config.filetypes
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-      return client.name
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+        return msg
     end
-  end
-  return msg
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return client.name
+        end
+    end
+    return msg
 end
 
 require('lualine').setup {
-  options = {
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
-    globalstatus = true,
-  },
-  sections = {
-    lualine_a = { '' },
-    lualine_b = { 'branch' },
-    lualine_c = { myfilename },
-    -- lualine_c = {
-    --   {
-    --     'filename',
-    --     file_status = true, -- Displays file status (readonly status, modified status)
-    --     path = 2, -- 0: Just the filename
-    --     shorting_target = 80, -- Shortens path to leave 40 spaces in the window
-    --     symbols = {
-    --       modified = '[+]', -- Text to show when the file is modified.
-    --       readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
-    --       unnamed = '[No Name]', -- Text to show for unnamed buffers.
-    --     },
-    --   },
-    -- },
-    lualine_x = { '' },
-    lualine_y = {
-      {
-        lsp_server,
-        icon = '',
-        -- If you want to give the LSP name color then use the line below and
-        -- change the hex code to the color you want:
-        -- color = {fg = '#ffffff', gui = 'bold'}
-      },
-    'diagnostics',
-    'lsp_progress'
+    options = {
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        globalstatus = true,
     },
-    lualine_z = {  'progress' },
-  },
+    sections = {
+        lualine_a = { 'branch' },
+        lualine_b = { myfilename },
+        lualine_c = { {
+            "overseer",
+            label = '', -- Prefix for task counts
+            colored = true, -- Color the task icons and counts
+            unique = false, -- Unique-ify non-running task count by name
+            name = nil, -- List of task names to search for
+            name_not = false, -- When true, invert the name search
+            status = nil, -- List of task statuses to display
+            status_not = false, -- When true, invert the status search
+        } },
+        lualine_x = { '' },
+        lualine_y = {
+            {
+                lsp_server,
+                icon = '',
+                -- If you want to give the LSP name color then use the line below and
+                -- change the hex code to the color you want:
+                -- color = {fg = '#ffffff', gui = 'bold'}
+            },
+            'diagnostics',
+            'lsp_progress'
+        },
+        lualine_z = { 'progress' },
+    },
 }
